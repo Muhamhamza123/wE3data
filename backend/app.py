625ee28a7d30cdf2d748486571667d5ef3a6e2d9
Muhamhamza123 <126794203+Muhamhamza123@users.noreply.github.com>
2024-01-03 14:29:28 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -25,8 +25,8 @@ from flask import Flask, send_from_directory
 app = Flask(
     __name__,
     static_url_path='',
-    static_folder='../w3data/build',
-    template_folder='../w3data/build'
+    static_folder='w3data/build',
+    template_folder='w3data/build'
 )
 
 # ... (other routes and configurations)
